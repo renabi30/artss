59f6d0aa7d92cef4ff43027d3f992198d06cc75d
Rena <renabi30@gmail.com>
2024-04-20 12:51:38 -0400

    update control

--- control/cis2.ino
+++ control/cis2.ino
@@ -1,93 +1,136 @@
 
-look here -> https://community.element14.com/products/arduino/b/blog/posts/simple-arduino-dc-motor-control-with-encoder-part-1
-//counter -> interrupts 
-volatile int counter= 0;
+// Pins used to control motors (TB6612FNG)
+const uint16_t PWMA = 5;         // Motor A PWM control
+const uint16_t AIN2 = 6;         // Motor A input 2
+const uint16_t AIN1 = 9;         // Motor A input 1
+const uint16_t STBY = 13;        // Standby
 
-//desired counter
+// Motor control pins for encoder feedback motor
+const int Input1 = 2;
+const int Input2 = 3;
+const int motorSpeedPin = 11;  // PWM pin for motor speed control
+
+// Constants
+const uint16_t ANALOG_WRITE_BITS = 8;
+const uint16_t MAX_PWM = pow(2, ANALOG_WRITE_BITS);
+const uint16_t MIN_PWM = MAX_PWM / 3;    // Make sure motor turns
+
+// Global variables for motor control with encoder feedback
+volatile int counter = 0;
 int desiredCounter = 0;
 
-//time 
-unsigned long previousTime = 0;
-const int outputDuration = 4;
-const int deltaT = 10;
+// Constants for tube advancement
+const float distancePerRevolution = 10.0; // Distance advanced per motor revolution (in cm)
+const float gearRatio = 50.0;             // Gear ratio of the motor
+const float countsPerRevolution = 12; // Counts per motor revolution (based on encoder),not multiplied by 4?
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(6, OUTPUT);
-  pinMode(5, OUTPUT);
-  digitalWrite(6, LOW);
-  digitalWrite(5, LOW);
+  // Initialize motors
+  initMotors();
+  pinMode(Input1, INPUT);
+  pinMode(Input2, INPUT);
+  pinMode(motorSpeedPin, OUTPUT);
 
-  //below is for the encoder 
-  pinMode(2, INPUT); //declare pins 2 and 3 as input
-  pinMode(3, INPUT);
-  attachInterrupt(0, ISR_A, CHANGE);  //attach interrupt to PIN2 triggered on change
-  attachInterrupt(1, ISR_B, CHANGE);  //attach interrupt to PIN3 triggered on change
+  // Attach interrupts for encoder feedback motor
+  attachInterrupt(digitalPinToInterrupt(Input1), ISR_A, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(Input2), ISR_B, CHANGE);
 
-  // Reset counter and set desired counter for a full revolution
-  counter = 0;
-  desiredCounter = 12 * 4 * 50; // 12 cycles per revolution, 4 counts per cycle, 50:1 gear ratio
+  // Calculate desired counter based on distance to move
+  float desiredDistance = 10.0; // Distance to move in cm
+  desiredCounter = (desiredDistance / distancePerRevolution) * countsPerRevolution * gearRatio;
 
- 
+  // Run the loop to move the tube
+  while (counter < desiredCounter) {
+    // Motor control with encoder feedback
+    int errorCounts = desiredCounter - counter;
+    float errorRadians = errorCounts * (2 * PI) / countsPerRevolution;
+    float controlSignal = 3.3 * errorRadians;
 
- float startTime = millis();
-
-// Run the step response loop in the setup
-  while (millis() - startTime < outputDuration * 1000) {
-
- 
-  int errorCounts = desiredCounter - counter;
-  float errorRadians = errorCounts * ( 2 * PI) / (12 * 4 * 50);
-
-
-
-//kprop is 3.3 can be adjusted, might not need kprop if not doing derivative control
-float controlSignal = 3.3 * errorRadians;
-
-
-//positive control signal, this is for 
-if (controlSignal >0 ) {
-  digitalWrite(Input1, HIGH); //need to declare Input1 and Input2 as eitherpin 5 or6
-  digitalWrite(Input2, LOW);
-}
-//negative control signal 
-if (controlSignal < 0) {
-  digitalWrite(Input1, LOW);
-  digitalWrite(Input2, HIGH);
-}
-// Display counter as a percent of a full rotation every 10th data point
-    if (counter % 10 == 0) {
-      float percentRotation = float(counter / (12 * 4 * 50)) * 100;
-      Serial.println(percentRotation);
+    // Motor direction control
+    if (controlSignal > 0) {
+      digitalWrite(AIN1, LOW);
+      digitalWrite(AIN2, HIGH);
+    } else {
+      digitalWrite(AIN1, HIGH);
+      digitalWrite(AIN2, LOW);
     }
+
+    // Set motor speed based on control signal
+    int speed = map(abs(controlSignal), 0, (int)(3.3 * (2 * PI)), 0, 255);
+    analogWrite(PWMA, speed);
+
     // Control the loop timing
-    delay(deltaT);
-}
-}
+    delay(10);
+  }
+
+  // After reaching the desired distance, stop the motor
+  brakeA();
 }
 
 void loop() {
- 
-/*
- digitalWrite(6, HIGH); //this reverses the tube out of the patient 
-  delay(1000000); //1000 seconds
-  digitalWrite(6, LOW);
-  */
-
-
+  // Your main loop code here
 }
-//interrupt methods
-void ISR_A() { //interrupt service routine 
-  if (digitalRead(3) == digitalRead(2)) { //if encoder a and b read the same
-    counter--; //subtract 1 from counter
-  } else {     //if encoder A and B are different
-    counter++; //add one to counter
+
+// Interrupt service routines for encoder feedback motor
+void ISR_A() {
+  if (digitalRead(Input2) == digitalRead(Input1)) {
+    counter--;
+  } else {
+    counter++;
   }
 }
+
 void ISR_B() {
-  if (digitalRead(3) == digitalRead(2)) { //if encoder A and B are the same
-    counter++; //add 1 to counter
-  } else { //if encoder A and B are different
-    counter--; //subtract 1 from counter
+  if (digitalRead(Input2) == digitalRead(Input1)) {
+    counter++;
+  } else {
+    counter--;
   }
+}
+
+// Functions for TB6612FNG motor control (same as before)
+void forwardA(uint16_t pwm) {
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, HIGH);
+  if (pwm > MAX_PWM) {
+    pwm = MAX_PWM;
+  }
+  if (pwm < MIN_PWM) {
+    pwm = MIN_PWM;
+  }
+  analogWrite(PWMA, pwm);
+}
+
+void reverseA(uint16_t pwm) {
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(AIN2, LOW);
+  if (pwm > MAX_PWM) {
+    pwm = MAX_PWM;
+  }
+  if (pwm < MIN_PWM) {
+    pwm = MIN_PWM;
+  }
+  analogWrite(PWMA, pwm);
+}
+
+void brakeA() {
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2, LOW);
+}
+
+void standbyMotors(bool standby) {
+  if (standby == true) {
+    digitalWrite(STBY, LOW);
+  } else {
+    digitalWrite(STBY, HIGH);
+  }
+}
+
+void initMotors() {
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+  pinMode(STBY, OUTPUT);
+  analogWriteResolution(ANALOG_WRITE_BITS);
+  standbyMotors(false);
 }
